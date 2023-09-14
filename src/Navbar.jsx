46091885a01b7e12f23b78ca9009f3bc8c43f828
thanachot30 <su.thanachot@gmail.com>
2023-09-14 08:33:22 +0700

    edit link

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,8 +1,11 @@
+import { Outlet, Link } from "react-router-dom";
 const Navbar = () =>{
     return (
         <div>
-            <a href="/">Home</a>
-            <a href="/Owner">Owner</a>
+            {/* <a href="/">Home</a> */}
+            <Link to="/">Home Link</Link>
+            <Link to="/Owner">Owner Link</Link>
+            {/* <a href="/Owner">Owner</a> */}
         </div>
     )
 }
